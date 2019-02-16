da6f45e974f517886c32489c89a64b2ba65287b5
See-Y <jyw10234@sasa.hs.kr>
2019-02-17 02:51:17 +0900

    initial settings

--- WebVR/models.py
+++ WebVR/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from colorful.fields import RGBColorField
 
 # Create your models here.
+class a_box(models.Model):
+    name = models.CharField(max_length=50)
+    color = RGBColorField(colors=['#FF0000', '#00FF00', '#0000FF'])
+    depth = models.FloatField()
+    height = models.FloatField()
+    width = models.FloatField()
+    src = models.CharField(max_length=100)
