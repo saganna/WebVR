f4d43a231330ac06258570500421be0566619159
See-Y <jyw10234@sasa.hs.kr>
2019-02-20 23:55:14 +0900

    shit

--- WebVR/models.py
+++ WebVR/models.py
@@ -4,42 +4,44 @@ from colorful.fields import RGBColorField
 # Create your models here.
 
 #Basic Models
-class Basemodel(Model, animation_class, position_class, rotation_class, scale_class, shadow_class):
+class Basemodel(Model):
     name = CharField(max_length=50)
     color = RGBColorField(colors=['#FF0000', '#00FF00', '#0000FF'])
     src = TextField()
     visible = BooleanField()
 
-class animation_class(Model):
-    pass
-
-class position_class(Model):
-    x=FloatField()
-    y=FloatField()
-    z=FloatField()
+    class ani():#animation class
     
-class rotation_class(Model):
-    x=FloatField()
-    y=FloatField()
-    z=FloatField()
-    
-class scale_class(Model):
-    x=FloatField()
-    y=FloatField()
-    z=FloatField()
+        pass
 
-class shadow_class(Model):
-    cast=BooleanField()
-    receive=BooleanField()
+    class pos():#position class
+        x=FloatField()
+        y=FloatField()
+        z=FloatField()
+
+    class rot():#rotation class
+        x=FloatField()
+        y=FloatField()
+        z=FloatField()
+    
+    class sca():#scale class
+        x=FloatField()
+        y=FloatField()
+        z=FloatField()
+    
+    class shadow():#shadow class
+        cast=BooleanField()
+        receive=BooleanField()
+
 
 
 #real object Models
-class a_box(Model, Basemodel):
+class a_box(Basemodel):
     depth = FloatField()
     height = FloatField()
     width = FloatField()
 
-class a_circle(Model, Basemodel):
-    radius=FloatField()
-    theta_length=FloatField()
-    theta_start=FloatField()
+class a_circle(Basemodel):
+    radius = FloatField()
+    theta_length = FloatField()
+    theta_start = FloatField()
